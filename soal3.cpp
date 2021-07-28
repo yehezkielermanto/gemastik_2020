60405d4da994207c36d9e0672e68b3292da7fef0
Yehezkiel Victorious Ermanto <64516825+yehezkielermanto@users.noreply.github.com>
2021-07-28 15:51:16 +0700

    Update soal3.cpp
    update code

--- soal3.cpp
+++ soal3.cpp
@@ -3,10 +3,16 @@
 
 using namespace std;
 
-int lipat(int panjang, int lebar, int ulang){
+string lipat(int panjang, int lebar, int ulang){
 	string hasil1,hasil2;
 	int temp;
 	int k=0;
+
+	// if(panjang % 2 !=0){
+	// 	panjang = panjang -1;
+	// }else if(lebar % 2 !=0){
+	// 	lebar = lebar -1;
+	// }
 
 	while(k<ulang){
 		if(panjang >= lebar){
@@ -18,16 +24,20 @@ int lipat(int panjang, int lebar, int ulang){
 		}
 		k+=1;
 	}
+	
 	//pertama kali muncul nilai yang besar
 	if(panjang<lebar){
 		temp = lebar;
 		lebar = panjang;
 		panjang = temp;
-		// hasil1 = to_string(panjang);
-		// hasil2 = to_string(lebar);
-		return panjang;
+		hasil1 = to_string(panjang);
+		hasil2 = to_string(lebar);
+		return hasil1 + " " + hasil2;
 		//cout<<panjang<<" "<<lebar;
 	}else{
+		hasil1 = to_string(panjang);
+		hasil2 = to_string(lebar);
+		return hasil1 + " " + hasil2;
 		// cout<<lebar<<" "<<panjang;
 	}
 }
@@ -37,7 +47,7 @@ int main(){
 	int simpan;
 	cin>>kertas;
 	
-	int krts[kertas];
+	string krts[kertas];
 	for(int i=0; i<kertas;i++){
 		cin>>P>>Q>>M;
 		krts[i] = lipat(P,Q,M);
